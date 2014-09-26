9a98b28a08b7830912f02022ca0d3e020b07a6c4
pedromateo <pedromateo@um.es>
2014-09-26 13:31:07 +0200

    improving rule loading

---
+++ sdaver_performance_test/testthread.cpp
@@ -0,0 +1,5 @@
+#include "testthread.h"
+
+TestThread::TestThread()
+{
+}
---
+++ sdaver_performance_test/testthread.h
@@ -0,0 +1,10 @@
+#ifndef TESTTHREAD_H
+#define TESTTHREAD_H
+
+class TestThread
+{
+public:
+    TestThread();
+};
+
+#endif // TESTTHREAD_H
